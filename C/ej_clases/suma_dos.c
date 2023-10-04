82bebb6561fc62294567da0337d57fbfd04cd46a
kupyy <collantes.jantonio@gmail.com>
2023-10-05 00:33:57 +0200

    Delete unnecesary files, write code on suma_dos.c

--- C/ej_clases/suma_dos.c
+++ C/ej_clases/suma_dos.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
-int main{
-    
+int main(){
+    int num1, num2, suma;
+
+    printf("Introduce un número: \n");
+    scanf("%d", &num1);
+
+    printf("Introduce el segundo número:\n");
+    scanf("%d", &num2);
+
+    suma=num1+num2;
+
+    printf("La suma de %d y %d da como resultado %d", num1, num2, suma);
 }
