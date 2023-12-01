f49b70bd48da7ac5b1fbb88852e7af36be77c64c
kupyy <collantes.jantonio@gmail.com>
2023-12-01 20:21:45 +0100

    Create new package

---
+++ Java/POO/Actividad3/src/act2/Movil.java
@@ -0,0 +1,11 @@
+package act2;
+
+public class Movil {
+	private String marca, modelo, procesador;
+	private double precio, 
+
+	public Movil() {
+		// TODO Apéndice de constructor generado automáticamente
+	}
+
+}
